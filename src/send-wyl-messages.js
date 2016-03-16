1ecc236203f90009150de73ffafaaa7a0e117e5e
Zachary Berardo <zachary@zachnthebox.com>
2016-03-15 20:35:31 -0400

    fix: readability and structure changes

--- src/send-wyl-messages.js
+++ src/send-wyl-messages.js
@@ -2,25 +2,25 @@ import postToFacebook from './post-to-facebook';
 import moment from 'moment';
 
 const firstMondayOfMonth = moment().startOf('month').day("Monday");
+
 const todayIsAfterFirstMondayOfMonth = moment().isAfter(firstMondayOfMonth);
-const lastMonday = moment().startOf('week').day('Monday').subtract(1, 'week').hour(8).minute(0);
+
+const previousMonday = moment().startOf('week').day('Monday').subtract(1, 'week').hour(8).minute(0);
 
 const changedLastWeek = event => {
   let updated = moment(event.updated);
   let created = moment(event.created);
-  return updated.isAfter(lastMonday) && !created.isAfter(lastMonday);
+  return updated.isAfter(previousMonday) && !created.isAfter(previousMonday);
 };
 
-const createdLastWeek = event => moment(event.created).isAfter(lastMonday);
+const createdLastWeek = event => moment(event.created).isAfter(previousMonday);
 
 export default function(events) {
   if (!events || !events.length) {
     throw false;
   }
 
-  let message = '"What You\'ve Learned" Assignments Coming Up:';
-  let eventMessages = '';
-
+  let eventMessages = '"What You\'ve Learned" Assignments Coming Up:';
   let eventsUpdated = false;
   let eventsCreated = false;
 
@@ -32,14 +32,14 @@ export default function(events) {
     eventsUpdated |= eventUpdated;
 
     let formattedDate = moment(event.start.date).format('LL');
-    eventMessages += `\n${formattedDate} - ${event.location}`;
+    let message = `\n${formattedDate} - ${event.location}`;
     if (eventUpdated) {
-      eventMessages += ' (Updated)';
+      message += ' (Updated)';
     }
+    eventMessages += message;
   });
 
   if (!todayIsAfterFirstMondayOfMonth || eventsUpdated || eventsCreated) {
-    message += eventMessages;
-    postToFacebook(message);
+    postToFacebook(eventMessages);
   }
 }
